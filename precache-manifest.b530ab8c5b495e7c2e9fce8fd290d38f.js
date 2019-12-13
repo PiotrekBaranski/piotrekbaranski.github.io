393c9d22a94311cb796c09aedb6b845a60765fde
Piotr Barański <piotreeeczek@gmail.com>
2019-12-13 19:56:44 +0100

    Updates

---
+++ precache-manifest.b530ab8c5b495e7c2e9fce8fd290d38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c960f08f020cf8fb2bc4cacafd7a870",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db72484a41d00da09b2",
+    "url": "/static/css/main.fc65d645.chunk.css"
+  },
+  {
+    "revision": "ca6106a1528990d4ad24",
+    "url": "/static/js/2.12a0941f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.12a0941f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2db72484a41d00da09b2",
+    "url": "/static/js/main.0778a6d5.chunk.js"
+  },
+  {
+    "revision": "b4518c1dbf4ced4caf36",
+    "url": "/static/js/runtime-main.b37a67e8.js"
+  }
+]);
